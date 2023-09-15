869a24dc472ca059283a1ffeedd3cce31a9bf77d
Rafael Cruz <rafaelcruz.net81@gmail.com>
2023-09-15 09:16:27 -0300

    criando projeto de revisao para AV1

---
+++ src/test/java/br/edu/ibmec/cloudcomputing/revisao/RevisaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ibmec.cloudcomputing.revisao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevisaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
